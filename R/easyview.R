95df26d20384009316a68f1b183f710f1bbc464b
tyler rinker <tyler_rinker@hotmail.com>
2012-07-31 00:09:49 -0400

    fixed bug

--- R/easyview.R
+++ R/easyview.R
@@ -3,6 +3,7 @@ function(sigsobject) {
     FUN <- function(x) {
         x <- ifelse(x==FALSE, "-", ifelse(x==TRUE, "WARNING", x)) 
         return(as.factor(x))
-    }
-    data.frame(sapply(sigsobject,  FUN), check.names=FALSE)
+    }  
+    data.frame(sigsobject[, 1, drop = FALSE], 
+        sapply(sigsobject,  FUN)[, -1], check.names=FALSE)
 }
