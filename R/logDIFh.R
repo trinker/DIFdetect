ab46bee443d35bc84d186c508c65f6b60800ebc3
tyler rinker <tyler_rinker@hotmail.com>
2012-07-30 23:25:58 -0400

    added documentation

--- R/logDIFh.R
+++ R/logDIFh.R
@@ -10,14 +10,20 @@ function(item, total, group, digits=3, output = "bare"){
     if (output == "full") {
         OP <- list("omnibus M3 to NULL"=anova(modn, mod2, test="Chisq"),
             "NULL"=summary(modn), "anova NULL" = anova(modn, test="Chisq"),  
-            "omnibus M1"=anova(modn, mod1, test="Chisq"), "anova M1" = anova(mod1, test="Chisq"), 
-            "betas M1" = TAB(mod1, digits=digits), "omnibus M2"=anova(mod1, mod2, test="Chisq"), 
-            "anova M2" = anova(mod2, test="Chisq"), "betas M2" = TAB(mod2, digits=digits))
+            "omnibus M1"=anova(modn, mod1, test="Chisq"), 
+            "anova M1" = anova(mod1, test="Chisq"), 
+            "betas M1" = TAB(mod1, digits=digits), 
+            "omnibus M2"=anova(mod1, mod2, test="Chisq"), 
+            "anova M2" = anova(mod2, test="Chisq"), 
+            "betas M2" = TAB(mod2, digits=digits))
     } else {
-        OP <- list("OMNIBUS BLOCK 1: DIF (INTERACTION to NULL)"=anova(modn, mod2, test="Chisq"), 
-            "OMNIBUS BLOCK 2: UNIFORM DIF (ADDIITIVE to NULL)"=anova(modn, mod1, test="Chisq"),  
+        OP <- list("OMNIBUS BLOCK 1: DIF (INTERACTION to NULL)"
+        	= anova(modn, mod2, test="Chisq"), 
+            "OMNIBUS BLOCK 2: UNIFORM DIF (ADDIITIVE to NULL)" 
+        	= anova(modn, mod1, test="Chisq"),  
             "betas BLOCK 2" = TAB(mod1, digits=digits), 
-            "OMNIBUS BLOCK 3: NON-UNIFORM DIF (INTERACTION TO ADDITIVE)"=anova(mod1, mod2, test="Chisq"), 
+            "OMNIBUS BLOCK 3: NON-UNIFORM DIF (INTERACTION TO ADDITIVE)" 
+        	= anova(mod1, mod2, test="Chisq"), 
             "betas BLOCK 3" = TAB(mod2, digits=digits))
     } 
     return(OP)
